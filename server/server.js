4d6cfe7bdf8db22521d21f03321dc2262477f5e5
Alaonit <alagiesaikou77@gmail.com>
2026-09-05 17:46:07 +0000

    Implement event ID filtering in server API and improve response handling

--- server/server.js
+++ server/server.js
@@ -4,9 +4,24 @@ import {events} from "../data/data.js"
 
 const PORT = 8000;
 const server = createServer((req,res)=>{
+  res.setHeader("Access-Control-Allow-Origin", "*")
   if(req.url === '/api/events' && req.method === 'GET'){
     res.setHeader('Content-Type','application/json');
     res.end(JSON.stringify(events))
+  }else{
+    res.statusCode = 404
+    res.end("Not found")
+  }
+
+  if(req.url === `/api/events/` && req.method === 'GET'){
+
+    const id = Number(req.url.split("/").pop ())
+
+    const filteredId = events.filter((event)=>{
+      return id === event.id;
+    })
+    res.setHeader('Content-Type','application/json');
+    res.end(JSON.stringify(filteredId))
   }
 })
 
